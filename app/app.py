879c5be2d3eb48a6ea8270cf866184ef0faa3d0d
Filipemont <filipemonteiro73@gmail.com>
2024-10-17 18:11:08 -0300

    Config do SqlAlchemy e do FlaskLogin

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from ext.database_ext import db
+from ext.login_manager import login_manager
+from flask import Flask  # type: ignore
+import config
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"postgresql://{config.POSTGRES_USER}:{config.POSTGRES_PASSWORD}"
+        f"@{config.POSTGRES_HOST}:{config.POSTGRES_PORT}/{config.POSTGRES_DB}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
